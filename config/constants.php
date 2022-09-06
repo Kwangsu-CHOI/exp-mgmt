c023d54e75372f0d40a685395257fe00925f6c40
Kwangsu-CHOI <wisecks@gmail.com>
2022-09-06 12:27:53 +1000

    set up for deploying
    change constants for heroku server

--- config/constants.php
+++ config/constants.php
@@ -2,10 +2,17 @@
 // ob_start();
 session_start();
 // ob_flush();
-define('ROOT_URL', 'http://localhost/exp-mgmt/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'wisecks');
-define('DB_PASS', 'Rhkdtn87**');
-define('DB_NAME', 'exp-mgmt');
+define('ROOT_URL', 'https://savemywallet.herokuapp.com/');
+define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
+define('DB_USER', 'b83d6c7735ada6');
+define('DB_PASS', '83047b42');
+define('DB_NAME', 'heroku_6472d9a6b16aae7');
 define('DB_TYPE', 'mysqli');
 
+
+// define('ROOT_URL', 'http://localhost/exp-mgmt/');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'wisecks');
+// define('DB_PASS', 'Rhkdtn87**');
+// define('DB_NAME', 'exp-mgmt');
+// define('DB_TYPE', 'mysqli');
